37194e0634e2bfae85dec8f29a36ddcdd3bc4e2b
nikosbakalis <bakalis.nikolas@gmail.com>
2022-10-22 18:14:05 +0100

    Renames

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from Model.config import config
 from Model import base
 
-from Model import Tickers, Countries
+from Model import tickers, countries
 
 if __name__ == '__main__':
     engine = create_engine("postgresql+psycopg2://" + config()['user'] + ":" + config()['password'] + "@" + config()['host'] + "/" + config()['database'] + "", echo=True)    
@@ -10,5 +10,5 @@ if __name__ == '__main__':
         with connection.begin():
             base.Base.metadata.create_all(engine, checkfirst=True)
 
-            ticker_creator = Tickers.Tickers()
-            countries_creator = Countries.Countries()
+            ticker_creator = tickers.Tickers()
+            countries_creator = countries.Countries()
