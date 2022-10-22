29a73d86610bcb0c6c589632a346e201c304cf44
nikosbakalis <bakalis.nikolas@gmail.com>
2022-10-22 01:36:58 +0100

    Two engine creations and some more ML.

---
+++ Model/Tickers.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Tickers(Base):
+    __tablename__ = 'Tickers'
+
+    tickerId = Column(Integer, primary_key=True)
+    tickerName = Column(String, nullable=False)
+    organizationName = Column(String)
+
+    def __repr__(self):
+        return f"Tickers(tickerId={self.tickerId!r}, tickerName={self.tickerName!r}, organizationName={self.organizationName!r})"
+
