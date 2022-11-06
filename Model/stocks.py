46c353923ff56762741e4bb0b2c802e1d59f88b4
nikosbakalis <bakalis.nikolas@gmail.com>
2022-11-06 13:48:29 +0000

    Fix __repr__

--- Model/stocks.py
+++ Model/stocks.py
@@ -17,4 +17,4 @@ class Stocks(Base):
     UniqueConstraint(tickerId, date)
 
     def __repr__(self):
-        return f'Stocks(stockId={self.stockId!r}, tickerId={self.tickerId} date={self.date!r}, open={self.open!r}, high={self.high!r}, low={self.low!r}, close={self.close!r}, adjclose={self.adjclose!r}, volume={self.volume!r})'
+        return f'Stocks(stockId={self.stockId!r}, tickerId={self.tickerId!r} date={self.date!r}, open={self.open!r}, high={self.high!r}, low={self.low!r}, close={self.close!r}, adjclose={self.adjclose!r}, volume={self.volume!r})'
