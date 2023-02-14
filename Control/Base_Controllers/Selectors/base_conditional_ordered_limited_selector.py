8331f7e080e4be4396e92c0d765b56932493228a
NikosBakalis <bakalis.nikolas@gmail.com>
2023-02-14 17:52:51 +0000

    Updated limited selector.

--- Control/Base_Controllers/Selectors/base_conditional_ordered_limited_selector.py
+++ Control/Base_Controllers/Selectors/base_conditional_ordered_limited_selector.py
@@ -9,7 +9,7 @@ def base_conditional_ordered_limited_selector(table, columnName, columnValue, or
         `columnName`: User must provide the `Name` of the `Column` that needs to be updated.
         `columnValue`: User must provide the `Value` of the `Column` `Where` the `Update` must happen.
         `orderColumn`: User must provide the `Name` of the `Column` on which the `Order By` must happen.
-        `limitNumber`: User must proveide the `Number` of the `Limit` of the results.
+        `limitNumber`: User must proveide the `Number` of the `Limit` of the results if negative, there is no limit.
 
     SQL query example:
         SELECT * FROM `table`
@@ -18,7 +18,10 @@ def base_conditional_ordered_limited_selector(table, columnName, columnValue, or
         LIMIT = `limitNumber`
     '''
     try:
-        return select(table).where(columnName == columnValue).order_by(orderColumn).limit(limitNumber)
+        if limitNumber >= 0:
+            return select(table).where(columnName == columnValue).order_by(orderColumn).limit(limitNumber)
+        else:
+            return select(table).where(columnName == columnValue).order_by(orderColumn)
     except:
         pass
         # print('Something went wrong with conditional ordered limited selection.') # TODO: This must be a logger.
